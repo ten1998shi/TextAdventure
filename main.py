ff9514dee66c072a6280dbd4ae8d5c6d9a66c9e7
unknown <tenshiamvs@gmail.com>
2024-10-05 23:12:38 +0200

    Added dics for Tier 1 to 3 Monster stats

--- main.py
+++ main.py
@@ -2,13 +2,175 @@
 #Tier 1 Mobs
 tinyRaptor = {
     "str": "5",
-    ""
+    "int": "1",
+    "vit": "3",
+    "dex": "7",
+    "spd": "7",
+    "ins": "1",
+    "def": "3",
+    "luck": "1"
+}
 
+caveSpider = {
+    "str": "3",
+    "int": "1",
+    "vit": "2",
+    "dex": "6",
+    "spd": "5",
+    "ins": "1",
+    "def": "2",
+    "luck": "1"
+}
+
+grublingWorm = {
+    "str": "1",
+    "int": "1",
+    "vit": "10",
+    "dex": "1",
+    "spd": "1",
+    "ins": "1",
+    "def": "10",
+    "luck": "1"
+}
+
+whiskerRat = {
+    "str": "3",
+    "int": "1",
+    "vit": "2",
+    "dex": "1",
+    "spd": "10",
+    "ins": "1",
+    "def": "4",
+    "luck": "1"
+}
+
+duskbatHatchling = {
+    "str": "1",
+    "int": "1",
+    "vit": "10",
+    "dex": "1",
+    "spd": "1",
+    "ins": "1",
+    "def": "10",
+    "luck": "1"
 }
 
 #Tier 2 Mobs
 
+squeakclawDrake = {
+    "str": "15",
+    "int": "5",
+    "vit": "10",
+    "dex": "5",
+    "spd": "10",
+    "ins": "5",
+    "def": "10",
+    "luck": "10"
+}
+
+frostlingSprite = {
+    "str": "5",
+    "int": "25",
+    "vit": "5",
+    "dex": "5",
+    "spd": "10",
+    "ins": "10",
+    "def": "5",
+    "luck": "10"
+}
+
+twitchfangBat = {
+    "str": "5",
+    "int": "5",
+    "vit": "5",
+    "dex": "5",
+    "spd": "40",
+    "ins": "5",
+    "def": "5",
+    "luck": "5"
+}
+
+feralGnat = {
+    "str": "20",
+    "int": "2",
+    "vit": "10",
+    "dex": "5",
+    "spd": "10",
+    "ins": "1",
+    "def": "10",
+    "luck": "1"
+}
+
+pufftailRabbit = {
+    "str": "10",
+    "int": "3",
+    "vit": "10",
+    "dex": "5",
+    "spd": "25",
+    "ins": "5",
+    "def": "10",
+    "luck": "20"
+}
+
 #Tier 3 Mobs
+
+emberjawDrake = {
+    "str": "35",
+    "int": "20",
+    "vit": "20",
+    "dex": "5",
+    "spd": "25",
+    "ins": "15",
+    "def": "25",
+    "luck": "10"
+}
+
+vilewingHarpy = {
+    "str": "15",
+    "int": "20",
+    "vit": "10",
+    "dex": "30",
+    "spd": "40",
+    "ins": "10",
+    "def": "10",
+    "luck": "10"
+}
+
+frostclawLynx = {
+    "str": "20",
+    "int": "10",
+    "vit": "20",
+    "dex": "5",
+    "spd": "30",
+    "ins": "5",
+    "def": "20",
+    "luck": "20"
+}
+
+fierceRavanger = {
+    "str": "40",
+    "int": "5",
+    "vit": "20",
+    "dex": "5",
+    "spd": "35",
+    "ins": "5",
+    "def": "10",
+    "luck": "10"
+}
+
+blightrootTreant = {
+    "str": "15",
+    "int": "10",
+    "vit": "40",
+    "dex": "5",
+    "spd": "5",
+    "ins": "5",
+    "def": "50",
+    "luck": "10"
+}
+
+
+
 
 #Tier 4 Mobs
 
